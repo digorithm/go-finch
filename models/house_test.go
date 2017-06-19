582a54e10cc22b43bc9afd3f8e807c12e4ca5da0
gulipek95 <candan.gulipek@gmail.com>
2017-06-19 15:21:28 -0700

    rebasing

--- models/house_test.go
+++ models/house_test.go
@@ -3,6 +3,7 @@ package models
 import (
 	_ "github.com/lib/pq"
 	"testing"
+	"fmt"
 )
 
 func newHouseForTest(t *testing.T) *House {
@@ -19,10 +20,18 @@ func TestHouseCRUD(t *testing.T) {
 		t.Errorf("Creating house should work. Error: %v", err)
 	}
 
+	users, err := h.GetHouseUsers(nil, 1)
+	if err != nil {
+		t.Errorf("Getting users should work. Error: %v", err)
+	}
+	
+	fmt.Println(users)
+
 	// Test deletion
 	_, err = h.DeleteById(nil, houseRow.ID)
 	if err != nil {
 		t.Fatalf("Deleting house by id should not fail. Error: %v", err)
 	}
-
 }
+
+
