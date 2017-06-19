9f84851cd214c2397e8ec42cb33c42c5d8f4f37a
gulipek95 <candan.gulipek@gmail.com>
2017-06-19 15:33:23 -0700

    extended UserRow with UserOwnTypeRow

--- models/rowStructs.go
+++ models/rowStructs.go
@@ -14,10 +14,7 @@ type UserRow struct {
 }
 
 type UserOwnTypeRow struct {
-	Id			int64 `db: "id"`
-	Email 		string `db: "email"`
-	Password	string `db: "password"`
-	Username	string `db: "username"`
+	UserRow
 	OwnType		int64  `db: "own_type"`
 	Description string `db: "description"`
 }
