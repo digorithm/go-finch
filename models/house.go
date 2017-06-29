2f9876b99caaf8c9d93db9aa9f140e852fb91af5
gulipek95 <candan.gulipek@gmail.com>
2017-06-29 15:02:02 -0700

    removed duplicated code, finalized migration to storage

--- models/house.go
+++ models/house.go
@@ -18,19 +18,6 @@ func NewHouse(db *sqlx.DB) *House {
 	return house
 }
 
-func NewItemInStorage(db *sqlx.DB) *ItemInStorage {
-	storage := &ItemInStorage{}
-	storage.db = db
-	storage.table = "ITEM_IN_STORAGE"
-	storage.hasID = true
-
-	return storage
-}
-
-type ItemInStorage struct {
-	Base
-}
-
 // House is a base
 type House struct {
 	Base
@@ -107,52 +94,6 @@ func (h *House) GetHouseRecipes(tx *sqlx.Tx, houseID int64) ([]RecipeRow, error)
 	query := "SELECT R.ID, R.NAME, R.TYPE, R.SERVES_FOR FROM RECIPE R INNER JOIN HOUSE_RECIPE H ON R.ID = H.RECIPE_ID WHERE H.HOUSE_ID = $1"
 
 	return h.GetRecipeForStruct(tx, query, houseID)
-}
-
-// GetHouseStorage retrieves all ingredients in a given house's storage and returns relevant information about the ingredients
-func (h *House) GetHouseStorage(tx *sqlx.Tx, houseID int64) ([]HouseStorageRow, error) {
-
-	query := "SELECT S.HOUSE_ID, I.ID, S.AMOUNT, S.UNIT_ID, I.NAME, U.NAME FROM INGREDIENT I INNER JOIN ITEM_IN_STORAGE S ON I.ID = S.INGREDIENT_ID INNER JOIN UNIT U ON U.ID = S.UNIT_ID WHERE S.HOUSE_ID = $1"
-
-	data, err := h.GetCompoundModel(tx, query, houseID)
-
-	storage := createHouseStorageRows(data)
-
-	if err != nil {
-		fmt.Printf("%v", err)
-	}
-
-	return storage, err
-}
-
-func (i *ItemInStorage) UpdateStorage(tx *sqlx.Tx, houseID int64, ingID int64, newAmt float64, newUnt int64) ([]HouseStorageRow, error) {
-
-	var storage []HouseStorageRow
-	var err error
-	return storage, err
-}
-
-func (i *ItemInStorage) InsertToStorage(tx *sqlx.Tx, houseID int64, ingID int64, amount float64, unitID int64) (ItemInStorageRow, error) {
-
-	data := make(map[string]interface{})
-	data["house_id"] = houseID
-	data["ingredient_id"] = ingID
-	data["amount"] = amount
-	data["unit_id"] = unitID
-
-	_, err := i.InsertIntoMultiKeyTable(tx, data)
-
-	if err != nil {
-		fmt.Printf("%v", err)
-	}
-
-	query := fmt.Sprintf("SELECT * FROM ITEM_IN_STORAGE WHERE HOUSE_ID = %v AND INGREDIENT_ID = $1", houseID)
-
-	res, err := i.GetCompoundModel(tx, query, ingID)
-
-	storage := createItemInStorage(res)
-
-	return storage, err
 }
 
 // UpdateHouseHold updates the number of residents in a given house by taking the number of households
